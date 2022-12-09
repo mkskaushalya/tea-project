7003744bf1a2f1444f70f5df5966df30bc3e09c9
Sahan Kaushalya <mkskaushalya@gmail.com>
2022-12-09 15:02:00 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+// html tag / title/ head tag 
+// css link
+include "inc/connect.php";
+include "inc/head.php";
+include "inc/functions.php";
+
+
+// Nav bar include
+include "inc/navbar.php";
+
+// footer and closing body and html tags
+include "inc/footer.php";
+?>
