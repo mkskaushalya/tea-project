d04aac04449b2d102fb41ad86d382f1a2256640c
Sahan Kaushalya <mkskaushalya@gmail.com>
2022-12-14 04:12:27 +0530

    Create login.php

---
+++ login.php
@@ -0,0 +1,25 @@
+<?php
+include "inc/head.php";
+
+?>
+
+<section class="login">
+
+    <form action="inc/action.php">
+        <h2>Login</h2>
+        <div class="item">
+            <input type="text" name="username" placeholder="Username">
+            <input type="password" name="password" placeholder="Password">
+        </div>
+        <button type="submit">Login</button>
+    </form>
+
+</section>
+
+
+
+
+<?php
+
+?>
+
