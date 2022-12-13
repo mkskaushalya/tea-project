98b30d01ea10c878f63d3ffc16c7cd32dceb40e1
Sahan Kaushalya <mkskaushalya@gmail.com>
2022-12-14 04:12:33 +0530

    Create signup.php

---
+++ signup.php
@@ -0,0 +1,29 @@
+<?php
+include "inc/head.php";
+
+?>
+
+<section class="login">
+
+    <form action="inc/action.php">
+        <h2>Signup</h2>
+        <div class="item">
+            <input type="text" name="firstname" placeholder="First Name">
+            <input type="text" name="lastname" placeholder="Last Name">
+            <input type="email" name="email" placeholder="Email">
+            <input type="tel" name="phone" placeholder="Phone Number">
+            <input type="text" name="username" placeholder="Username">
+            <input type="password" name="password" placeholder="Password">
+        </div>
+        <button type="submit">Signup</button>
+    </form>
+
+</section>
+
+
+
+
+<?php
+
+?>
+
