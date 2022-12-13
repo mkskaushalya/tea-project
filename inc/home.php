0fec16148aad901c70883ff6df0bd9635878d29d
Sahan Kaushalya <mkskaushalya@gmail.com>
2022-12-14 04:12:06 +0530

    Create home.php

---
+++ inc/home.php
@@ -0,0 +1,12 @@
+<section class="home">
+    <div class="left">
+        <h1>Tea Management System</h1>
+        <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+        </p>
+    </div>
+
+    <div class="right">
+        <img src="img/tea-leaf.jpg" alt="">
+    </div>
+</section>
