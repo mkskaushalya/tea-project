11b3d7e4bd9a67770de252e4d7fb9c0e1e80e64d
Sahan Kaushalya <mkskaushalya@gmail.com>
2022-12-14 04:12:12 +0530

    Create navbar.php

---
+++ inc/navbar.php
@@ -0,0 +1,25 @@
+<nav>
+    <div class="left">
+        <div class="logo">Tea</div>
+        <div class="brand">Management System</div>
+    </div>
+    <div class="right">
+        <ul>
+            <a href="index.php">
+                <li>Home</li>
+            </a>
+            <a href="#">
+                <li>About</li>
+            </a>
+            <a href="#">
+                <li>Contact</li>
+            </a>
+            <a href="login.php">
+                <li>Login</li>
+            </a>
+            <a href="signup.php">
+                <li>Signup</li>
+            </a>
+        </ul>
+    </div>
+</nav>
