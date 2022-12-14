123a9f68c9d0e84352514278f0d12cd07bc3993c
Sahan Kaushalya <mkskaushalya@gmail.com>
2022-12-15 00:08:54 +0530

    Create product.php

---
+++ product.php
@@ -0,0 +1,87 @@
+<?php
+include "inc/head.php";
+?>
+
+<section class="product" style="padding-top:100px;">
+
+    <div class="card">
+        <form action="">
+        <h3>Product Name</h3>
+        <div class="image">
+            <img src="img/tea-leaf.jpg" alt="">
+        </div>
+        <div class="price">
+            <span>Rs. 10,000</span>
+        </div>
+        <div class="quantity">
+            <label for="quantity">Quantity</label>
+            <input type="number" name="quantity" id="quantity">
+        </div>
+
+        <p>Type: <br>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores suscipit a ad aliquid voluptatum illum autem debitis in labore harum!</p>
+        <button type="submit">Buy</button>
+        </form>
+    </div>
+
+    <div class="card">
+        <form action="">
+        <h3>Product Name</h3>
+        <div class="image">
+            <img src="img/tea-leaf.jpg" alt="">
+        </div>
+        <div class="price">
+            <span>Rs. 10,000</span>
+        </div>
+        <div class="quantity">
+            <label for="quantity">Quantity</label>
+            <input type="number" name="quantity" id="quantity">
+        </div>
+
+        <p>Type: <br>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores suscipit a ad aliquid voluptatum illum autem debitis in labore harum!</p>
+        <button type="submit">Buy</button>
+        </form>
+    </div>
+
+    <div class="card">
+        <form action="">
+        <h3>Product Name</h3>
+        <div class="image">
+            <img src="img/tea-leaf.jpg" alt="">
+        </div>
+        <div class="price">
+            <span>Rs. 10,000</span>
+        </div>
+        <div class="quantity">
+            <label for="quantity">Quantity</label>
+            <input type="number" name="quantity" id="quantity">
+        </div>
+
+        <p>Type: <br>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores suscipit a ad aliquid voluptatum illum autem debitis in labore harum!</p>
+        <button type="submit">Buy</button>
+        </form>
+    </div>
+
+    <div class="card">
+        <form action="">
+        <h3>Product Name</h3>
+        <div class="image">
+            <img src="img/tea-leaf.jpg" alt="">
+        </div>
+        <div class="price">
+            <span>Rs. 10,000</span>
+        </div>
+        <div class="quantity">
+            <label for="quantity">Quantity</label>
+            <input type="number" name="quantity" id="quantity">
+        </div>
+
+        <p>Type: <br>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores suscipit a ad aliquid voluptatum illum autem debitis in labore harum!</p>
+        <button type="submit">Buy</button>
+        </form>
+    </div>
+
+</section>
