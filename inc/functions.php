bb1401d9be346087c8f80f79ae7b4797f4116dae
Sahan Kaushalya <mkskaushalya@gmail.com>
2022-12-15 02:00:38 +0530

    Update functions.php

--- inc/functions.php
+++ inc/functions.php
@@ -88,22 +88,79 @@ function productType($conn){
 function sell($title, $price, $description, $quantity, $type, $userid, $conn){
   $sell = null;
   $msg = null;
-  $query = "INSERT INTO users(userid,	title,	price,	description, quantity,	status,	type) VALUES('$userid', '$title', '$price', '$description', '$quantity',  1, $type);";
-      // echo $sqlq;
-      if ($conn->query($sqlq) === TRUE) {//record added
-        $msg = "New user record created successfully<br>Registeration Successfull!..<br>Now you can login in our system";
-        $signup = true;
-      }else{
-        $msg = "No record found...<br>";
-        $signup = false;
-      }
-
-    }
+  $query = "INSERT INTO product(userid,	title,	price,	description, quantity,	status,	type, image) VALUES('$userid', '$title', '$price', '$description', '$quantity',  1, $type, 'img/tea-leaf.jpg');";
+  if ($conn->query($query) === TRUE) {//record added
+    $msg = "New Item aded successfully";
+    $sell = true;
   }else{
     $msg = "Database Error";
-    $signup = false;
-
+    $sell = false;
   }
-  return array("firstname" => $firstname, "lastname" => $lastname, "status" => true, "signup" =>  $signup, "msg" => $msg,"username" => $username, "email" => $email, "phone" => $phone);
+  return array("query" => $query, "sell" => $sell, "title" => $title, "price" => $price, "description" =>  $description, "msg" => $msg,"quantity" => $quantity, "type" => $type);
+}
+
+
+function Product($type, $conn){  
+  $query = "SELECT * FROM product WHERE type='$type'";
+  $query_run = mysqli_query($conn, $query);
+  $print_data = "";    
+  if(mysqli_num_rows($query_run) > 0){
+    $record = false;
+    foreach($query_run as $items){
+        $print_data .='<div class="card"><form action="inc/action.php"><h3>'. $items['title'] .'</h3>';
+        $print_data .='<div class="image">';
+        $print_data .='<img src="'. $items['image'] .'" alt="">';
+        $print_data .='</div>';
+        $print_data .='<div class="price">';
+        $print_data .='<span>Rs. '. $items['price'] .'</span>';
+        $print_data .='</div>';
+        $print_data .='<div class="quantity">';
+        $print_data .='<label for="quantity">Quantity</label>';
+        $print_data .='<input type="number" name="quantity" id="quantity" max="'. $items['quantity'] .'">';
+        $print_data .='</div>';
+        $print_data .='<p>Type: '. $items['type'] .'<br>';
+        $print_data .= $items['description'];
+        $print_data .='<button type="submit" name="buy" value="'. $items['id'] .'">Buy</button>';
+        $print_data .='</form>';
+        $print_data .='</div>';
+        $record = true;
+    }
+  }
+  return array("record" => $record, 'print_data' => $print_data);
+}
+
+
+function addItemTable($userid, $conn){
+  $query = "SELECT * FROM product WHERE userid=$userid ORDER BY datetime DESC";
+  $query_run = mysqli_query($conn, $query);
+  $tbody = "";
+  if(mysqli_num_rows($query_run) > 0){
+    foreach($query_run as $items){
+      $id = $items['id'];
+      $title = $items['title'];
+      $price = $items['price'];
+      $description = $items['description'];
+      $quantity = $items['quantity'];
+      $type = $items['type'];
+      $query2 = "SELECT * FROM producttype WHERE id='$type' LIMIT 1";
+      $result2 = mysqli_query($conn, $query2);
+      if($result2)	{
+        if($result2 && mysqli_num_rows($result2) > 0)	{
+          $userdata = mysqli_fetch_assoc($result2);
+          $productname = $userdata['name'];
+          $image = $userdata['image'];
+        }
+      }
+      $adedtime = strtotime($items['datetime']);
+      $tbody .= '<tr>';
+      $tbody .= '<td>'.$id.'</td><td>'.$title.'</td><td>'.$price.'</td><td>'.$description.'</td><td>'.$quantity.'</td><td>'.$productname.'</td><td>'.date("Y-m-d", $adedtime).'</td><td>'.date("H:i:s", $adedtime).'</td><td><form action="inc/action.php" method="post"><button type="submit" class="edit" value="'.$id.'" name="productedit">Edit</button></form></td><td><form action="inc/action.php" method="post"><button class="delbutton" type="submit" value="'.$id.'" name="productdelete">Delete</button></form></td>';
+      $tbody .= '</tr>';
+  
+    }
+  return $tbody;
+
+  }else{
+    return "<tr><td colspan='9'>No Data.</td></tr>";
+}
 }
 ?>
