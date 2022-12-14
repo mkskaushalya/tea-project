d367b8ab3ca33ab0761be84d495e73b8413543d0
Sahan Kaushalya <mkskaushalya@gmail.com>
2022-12-14 15:45:08 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,17 +1,20 @@
 <?php
 include "inc/head.php";
+if(isset($_SESSION['user_data'])){
+	header("Location: index.php?msg=already-logged");
+}
 
 ?>
 
 <section class="login">
 
-    <form action="inc/action.php">
+    <form action="inc/action.php" method="post">
         <h2>Login</h2>
         <div class="item">
-            <input type="text" name="username" placeholder="Username">
-            <input type="password" name="password" placeholder="Password">
+            <input type="text" name="username" placeholder="Username" required autocomplete="off">
+            <input type="password" name="password" placeholder="Password" required autocomplete="off">
         </div>
-        <button type="submit">Login</button>
+        <button type="submit" name="login">Login</button>
     </form>
 
 </section>
