cb289be07fab772593dcd277dac764ef3cf171f6
Sahan Kaushalya <mkskaushalya@gmail.com>
2022-12-16 03:21:36 +0530

    Create product.php

---
+++ inc/product.php
@@ -0,0 +1,43 @@
+<h3 style="text-align: center; width:100%; padding-top:50px">Tea Plants</h3>
+
+<section class="product" style="padding-top:50px;">
+
+<?php
+
+echo Product(1, $conn)['print_data'];
+
+?>
+</section>
+
+<h3 style="text-align: center; width:100%; padding-top:50px">Fresh Tea Leaves</h3>
+
+<section class="product" style="padding-top:50px;">
+
+<?php
+
+echo Product(2, $conn)['print_data'];
+
+?>
+</section>
+
+<h3 style="text-align: center; width:100%; padding-top:50px">DRY Tea Leaves</h3>
+<section class="product" style="padding-top:50px;">
+
+<?php
+
+echo Product(3, $conn)['print_data'];
+
+?>
+
+</section>
+
+<h3 style="text-align: center; width:100%; padding-top:50px">Tea Product</h3>
+<section class="product" style="padding-top:50px;">
+
+
+<?php
+
+echo Product(4, $conn)['print_data'];
+
+?>
+</section>
